bfe753704abe97f7de6a780b369f7b103242a241
Gatsjy <gkswndks123@naver.com>
2020-03-29 17:49:52 +0900

    Create 2155.cpp
    2020-02-29, 한주안, 2155

---
+++ DP/2155.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+long long dp[201][201] = { 0 };
+int mod = 1000000000;
+
+int main() {
+
+	int n, k;
+
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i <= n; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= k; i++) {
+		for (int j = 0; j <= n; j++) {
+			for (int k = 0; k <= j; k++) {
+				dp[i][j] += dp[i - 1][k];
+			}
+			dp[i][j] %= mod;
+		}
+	}
+
+	printf("%lld", dp[k][n]);
+}
