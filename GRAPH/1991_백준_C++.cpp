e1c199dcfc29a7b8e47e71fe5d500a48ac99d921
Gatsjy <gkswndks123@naver.com>
2020-04-14 20:58:47 +0900

    Create 1991_백준_C++.cpp

---
+++ GRAPH/1991_백준_C++.cpp
@@ -0,0 +1,58 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int tree[26][2];
+
+void pre(int n) {
+	if (n == -1) return;
+	cout << char(n + 'A');
+	pre(tree[n][0]);
+	pre(tree[n][1]);
+}
+
+void in(int n) {
+	if (n == -1) return;
+	in(tree[n][0]);
+	cout << char(n + 'A');
+	in(tree[n][1]);
+}
+
+void post(int n) {
+	if (n == -1) return;
+	post(tree[n][0]);
+	post(tree[n][1]);
+	cout << char(n + 'A');
+}
+
+int main() {
+	int n;
+	cin >> n;
+	while (n--) {
+		char node, left, right;
+
+		cin >> node >> left >> right;
+
+		if (left != '.') {
+			tree[node - 'A'][0] = left - 'A';
+		}
+		else {
+			tree[node - 'A'][0] = -1;
+		}
+
+		if (right != '.') {
+			tree[node - 'A'][1] = right - 'A';
+		}
+		else {
+			tree[node - 'A'][1] = -1;
+		}
+	}
+
+	pre(0);
+	cout << endl;
+	in(0);
+	cout << endl;
+	post(0);
+}
