968d75ce3e5d310cc84c472c028ac9c625e1ac46
Gatsjy <gkswndks123@naver.com>
2020-04-18 21:09:04 +0900

    2020-04-18, 한주안, commit

---
+++ 기타/1919_백준_C++.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+int ch[26];
+int ch2[26];
+
+int main() {
+	
+	string s;
+	string s2;
+
+	cin >> s >> s2;
+
+	for (int i = 0; i < s.size(); i++) {
+		ch[s[i] - 'a']++;
+	}
+
+	for (int i = 0; i < s2.size(); i++) {
+		ch2[s2[i] - 'a']++;
+	}
+	int cnt = 0;
+	for (int i = 0; i < 26; i++) {
+		cnt += abs(ch[i] - ch2[i]);
+	}
+
+	cout << cnt;
+}
---
+++ 기타/5397_백준_C++(stack이용해서키로그만들기).cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+
+	cin >> n;
+
+	while (n--) {
+		string s;
+
+		stack<char> pass;
+		stack<char> tmppass;
+		
+		cin >> s;
+
+		int idx = 0;
+		
+		for (int i = 0; s[i] != '\0'; i++) {
+			if (s[i] == '<') {
+				if (!pass.empty()) {
+					tmppass.push(pass.top());
+					pass.pop();
+				}
+			}
+			else if (s[i] == '>'){
+				if (!tmppass.empty()) {
+					pass.push(tmppass.top());
+					tmppass.pop();
+				}
+			}
+			else if (s[i] == '-') {
+				if (!pass.empty()) {
+					pass.pop();
+				}
+			}
+			else {
+				pass.push(s[i]);
+			}
+		}
+
+		while (!tmppass.empty()) {
+			pass.push(tmppass.top());
+			tmppass.pop();
+		}
+
+		string result = "";
+		while(!pass.empty()){
+			result.push_back(pass.top());
+			pass.pop();
+		}
+
+		reverse(result.begin(), result.end());
+		cout << result << endl;
+	}
+}
