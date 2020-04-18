58f7e34a006f3513dcaa2354e2ec5e2ee918912d
Gatsjy <gkswndks123@naver.com>
2020-04-18 16:38:36 +0900

    2020-04-18, 한주안, commit

---
+++ 기타/11328_백준_C++.cpp
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+int sarr[26];
+int nsarr[26];
+
+// 11328/백준/C++
+// write by gatsjy
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	while (n--) {
+
+		// 체크배열 초기화
+		memset(sarr, 0, sizeof(sarr));
+		memset(nsarr, 0, sizeof(nsarr));
+
+		string s;
+		string ns;
+
+		cin >> s >> ns;
+
+		for (int i = 0; s[i] != '\0'; i++) {
+			sarr[s[i] - 'a']++;
+			nsarr[ns[i] - 'a']++;
+		}
+
+		bool flag = true;
+		// checking
+		for (int i = 0; i < 26; i++) {
+			if (sarr[i] != nsarr[i]) {
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag) {
+			cout << "Possible" << endl;
+		}
+		else {
+			cout << "Impossible" << endl;
+		}
+		
+	}
+}
---
+++ 기타/13300_백준_C++.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+int room[7][2];
+int roomcnt = 0;
+int n, k;
+
+// 13300/백준/C++
+// written by Gatsjy
+int main() {
+	// n : 참가학생수 , k : 한방 최대 인원 수
+	// 0 : 여학생 , 1 : 남학생
+	cin >> n >> k;
+
+	while (n--) {
+		int s, y;
+		cin >> s >> y;
+		room[y][s]++;
+	}
+
+	for (int i = 1; i < 7; i++) {
+		for (int j = 0; j < 2; j++) {
+			if (room[i][j] % k == 0) {
+				roomcnt += room[i][j] / k;
+			}
+			else {
+				roomcnt += room[i][j] / k + 1;
+			}
+		}
+	}
+
+	cout << roomcnt;
+}
