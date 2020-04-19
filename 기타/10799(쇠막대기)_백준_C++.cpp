18485555fcbb1e6c15be7105d164bb84edd0f343
Gatsjy <gkswndks123@naver.com>
2020-04-19 19:51:12 +0900

    2020-04-19, 한주안, commit

---
+++ 기타/10799(쇠막대기)_백준_C++.cpp
@@ -0,0 +1,58 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+// 10799/백준/C++
+// written by Gatyjy
+// 레이저를 쏘는 당시에 stack의 사이즈를 확인해서 그만큼의 막대기가 추가된다.
+// 하나의 막대기가 끝나는 시점에는 하나의 막대기가 생긴다(연결이 끊어지기 때문에)
+int main() {
+
+	string s;
+	cin >> s;
+
+	stack<char> stack;
+	bool flag = true;
+
+	int cnt = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (!stack.empty()) {
+			if (s[i] == ')') {
+				if (stack.top() == '(') {
+					if (s[i - 1] == '(') {
+						stack.pop();
+						cnt += stack.size();
+					}
+					else {
+						stack.pop();
+						cnt += 1;
+					}
+				}
+				else {
+					stack.pop();
+					cnt += 1;
+				}
+			}
+			else {
+				stack.push('(');
+			}
+		}
+		else {
+			if (s[i] == ')') {
+				flag = false;
+				break;
+			}
+			else {
+				stack.push('(');
+			}
+		}
+	}
+
+	cout << cnt;
+}
---
+++ 기타/9012(괄호)_백준_C++.cpp
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	while (n--) {
+		string s;
+		cin >> s;
+
+		stack<char> stack;
+		bool flag = true;
+		for (int i = 0; i < s.size(); i++) {
+			if (!stack.empty()) {
+				if (s[i] == ')') {
+					if (stack.top() == '(') {
+						stack.pop();
+					}
+					else {
+						flag = false;
+						break;
+					}
+				}
+				else {
+					stack.push('(');
+				}
+			}
+			else {
+				if (s[i] == ')') {
+					flag = false;
+					break;
+				}
+				else {
+					stack.push('(');
+				}
+			}
+		}
+
+		if (stack.empty()) {
+			if (flag) {
+				cout << "YES" << endl;
+			}
+			else {
+				cout << "NO" << endl;
+			}
+		}
+		else {
+			cout << "NO" << endl;
+		}
+	}
+}
