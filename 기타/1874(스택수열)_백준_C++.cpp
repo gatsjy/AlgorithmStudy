cbbd3d95d4489a99e610c13d89968a96c9a26447
Gatsjy <gkswndks123@naver.com>
2020-04-21 22:02:58 +0900

    Create 1874(스택수열)_백준_C++.cpp

---
+++ 기타/1874(스택수열)_백준_C++.cpp
@@ -0,0 +1,43 @@
+﻿#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+stack<int> st;
+vector<char> v;
+int a[100001];
+int main() {
+	
+	int n;
+
+	cin >> n;
+
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	int idx = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		st.push(i);
+		v.push_back('+');
+		while (!st.empty() && st.top() == a[idx]) {
+			st.pop();
+			v.push_back('-');
+			idx++;
+		}
+	}
+
+	if (st.empty()) {
+		for (char c : v) {
+			printf("%c\n", c);
+		}
+	}
+	else {
+		printf("NO");
+	}
+	
+	
+}
