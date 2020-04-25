0906d88e2b0419b9756a7c870654ad75fd7b2bf3
Gatsjy <gkswndks123@naver.com>
2020-04-25 22:34:40 +0900

    2020-04-25, 한주안, commit

---
+++ 기타/6198(옥상정원꾸미기)_백준_C++.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> tower;
+stack<pair<int,int>> st;
+int main(){
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int input;
+		scanf("%d", &input);
+		tower.push_back(input);
+	}
+
+	reverse(tower.begin(), tower.end());
+
+	st.push({ 0, tower[0] });
+	long long rsv = 0;
+	for (int i = 1; i < n; i++) {
+		if (!st.empty()) {
+			int cnt = 0;
+			while (!st.empty() && st.top().second < tower[i]) {
+				auto cur = st.top();
+				st.pop();
+				cnt += cur.first+1;
+			}
+			st.push({ cnt, tower[i] });
+			rsv += cnt;
+		}
+		else {
+			st.push({ 0,tower[i] });
+		}
+	}
+
+	cout << rsv;
+}
