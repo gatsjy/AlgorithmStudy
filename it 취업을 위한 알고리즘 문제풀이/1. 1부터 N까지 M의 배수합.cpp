62e5bcb6660f01d49a681d9ea49e7a5909c63fff
Gatsjy <gkswndks123@naver.com>
2020-05-11 19:09:44 +0900

    Create 1. 1부터 N까지 M의 배수합.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/1. 1부터 N까지 M의 배수합.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	int n, m;
+
+	scanf("%d %d", &n, &m);
+
+	int res = 0;
+	for (int i = 1; i <= n; i++) {
+		if (i % m == 0) {
+			res += i;
+		}
+	}
+	printf("%d", res);
+}
