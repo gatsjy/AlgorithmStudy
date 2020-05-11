bd2f25d053e9478aade2d6686cf0bf149a4a0cc2
Gatsjy <gkswndks123@naver.com>
2020-05-11 22:00:26 +0900

    Create 05. 나이계산.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/05. 나이계산.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+
+	cin >> s;
+	char sex;
+	int age = (s[0] - 48) * 10 + s[1] - 48;
+
+	if (s[7] - 48 == 1 || s[7] - 48 == 3) {
+		sex = 'M';
+	}
+	else {
+		sex = 'W';
+	}
+
+	if (s[7] - 48 == 3 || s[7] - 48 == 4) {
+		age = 20 - age;
+	}
+	else {
+		age = 100 - age + 20;
+	}
+	printf("%d %c", age, sex);
+	
+}
