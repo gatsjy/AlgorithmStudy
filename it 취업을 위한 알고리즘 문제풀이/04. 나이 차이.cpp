5e3992f294bd9da72cd98b0cdda238111ecb20ac
Gatsjy <gkswndks123@naver.com>
2020-05-11 19:41:04 +0900

    Create 04. 나이 차이.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/04. 나이 차이.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n, i, a, max = -2147000000, min = 2147000000;
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		cin>>a;
+		if (a > max) max = a;
+		if (a < min) min = a;
+	} 
+
+	printf("%d", max - min);
+}
