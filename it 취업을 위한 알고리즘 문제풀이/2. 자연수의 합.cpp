aaf11483ba8eded70d558af00bae3b19ec57839a
Gatsjy <gkswndks123@naver.com>
2020-05-11 19:13:50 +0900

    Create 2. 자연수의 합.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/2. 자연수의 합.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	int n, m;
+
+	scanf("%d %d", &n, &m);
+
+	int res = 0;
+	for (int i = n; i <= m; i++) {
+		if (i != n) {
+			printf("+");
+		}
+		printf("%d ", i);
+		res += i;
+	}
+	printf("= %d", res);
+}
