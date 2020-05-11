3268a1fb613c5f7708c63541245213a099423f2e
Gatsjy <gkswndks123@naver.com>
2020-05-11 23:03:52 +0900

    Create 08. 올바른 괄호.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/08. 올바른 괄호.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	stack<char> st;
+
+	char c[31];
+
+	scanf("%s", &c);
+
+	int i;
+
+	bool flag = true;
+	
+	for (i = 0; c[i] != '\0'; i++) {
+		if (c[0] == ')') {
+			flag = false;
+			break;
+		}
+		else {
+			if (c[i] == '(') {
+				st.push('(');
+			}
+			else {
+				if (!st.empty()) {
+					st.pop();
+				}
+				else {
+					flag = false;
+					break;
+				}
+			}
+		}
+	}
+
+	if (st.empty() && flag) {
+		printf("YES");
+	}
+	else {
+		printf("NO");
+	}
+}
