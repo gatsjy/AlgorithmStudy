c79cf52b4e699c315f64ed0951e6e6c990b40243
Gatsjy <gkswndks123@naver.com>
2020-05-11 22:55:48 +0900

    Create 07. 영어단어 복구.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/07. 영어단어 복구.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	char a[101];
+	char b[101];
+	int i, idx = 0;
+
+	gets_s(a);
+
+	for (i = 0; a[i] != '\0'; i++) {
+		if (a[i] == ' ') {
+			continue;
+		}
+		if (a[i] >= 'A' && a[i] <= 'Z') {
+			b[idx++] = a[i] + 32;
+		}
+		else {
+			b[idx++] = a[i];
+		}
+	}
+
+	b[idx] = '\0';
+	printf("%s", b);
+}
