7ce54648081bc44248937c7bfded5373dce05146
Gatsjy <gkswndks123@naver.com>
2020-05-12 22:56:47 +0900

    Create 14. 뒤집은 소수.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/14. 뒤집은 소수.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+using namespace std;
+int a[101];
+int n;
+int reverse(int i) {
+	int res = 0;
+	while (i > 0) {
+		res = res * 10 + i % 10;
+		i /= 10;
+	}
+	return res;
+}
+
+bool isPrime(int n) {
+	if (n == 1) return false;
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+int main() {
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		int tmp = reverse(a[i]);
+		if (isPrime(tmp)) {
+			printf("%d ", tmp);
+		}
+	}
+	
+}
