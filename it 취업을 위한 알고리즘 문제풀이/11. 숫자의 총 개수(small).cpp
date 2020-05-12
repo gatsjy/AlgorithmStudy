4f3e3ab54d7d31f6e2fb5aa89f70c17ef3a67e00
Gatsjy <gkswndks123@naver.com>
2020-05-12 20:52:41 +0900

    Create 11. 숫자의 총 개수(small).cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/11. 숫자의 총 개수(small).cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+using namespace std;
+
+int cntN(int n) {
+	int res = 0;
+	while (n > 0) {
+		n /= 10;
+		res++;
+	}
+	return res;
+}
+int main() {
+
+	int cnt = 0;
+	int n;
+
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		cnt += cntN(i);
+	}
+
+	printf("%d", cnt);
+}
