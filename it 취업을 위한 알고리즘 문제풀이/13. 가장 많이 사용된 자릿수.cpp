74a34c60cc1eb75829aa04ebaddfcedf276803a7
Gatsjy <gkswndks123@naver.com>
2020-05-12 22:19:23 +0900

    Create 13. 가장 많이 사용된 자릿수.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/13. 가장 많이 사용된 자릿수.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+using namespace std;
+char a[101];
+int cnt[10];
+
+int main() {
+
+	scanf("%s", &a);
+
+	for (int i = 0; a[i] != '\0'; i++) {
+		cnt[a[i] - 48] ++;
+	}
+
+	int max = -2147000000;
+	int idx = 0;
+
+	for (int i = 0; i < 10; i++) {
+		if (cnt[i] >= max) {
+			max = cnt[i];
+			idx = i;
+		}
+	}
+
+	printf("%d", idx);
+	
+}
