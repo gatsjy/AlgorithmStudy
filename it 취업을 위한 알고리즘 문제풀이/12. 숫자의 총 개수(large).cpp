8b503e8856ad108a0059b29ee8496e7e8380fcf8
Gatsjy <gkswndks123@naver.com>
2020-05-12 21:53:57 +0900

    Create 12. 숫자의 총 개수(large).cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/12. 숫자의 총 개수(large).cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+	int n, sum = 0, c = 1, d = 9, res = 0;
+
+	scanf("%d", &n);
+
+	// 이 생각을 해낼 줄 알아야 실력이 는다.                                                                                                                             
+	while (sum + d < n) {
+		res = res + (c * d);
+		sum = sum + d;
+		c++;
+		d *= 10;
+	}
+
+	res = res +((n - sum) * c);
+	printf("%d", res);
+}
