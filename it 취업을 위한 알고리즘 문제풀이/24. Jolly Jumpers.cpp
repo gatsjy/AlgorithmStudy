5adcbfaf438be55c4bf0e6825537ba3d81d9efc4
Gatsjy <gkswndks123@naver.com>
2020-05-16 23:21:27 +0900

    Create 24. Jolly Jumpers.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/24. Jolly Jumpers.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int i, n;
+	
+	scanf("%d", &n);
+
+	vector<int> a(n+1);
+	vector<int> b(n + 1);
+	
+	for (i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (i = 1; i < n; i++) {
+		if (abs(a[i] - a[i - 1]) <= 1000) {
+			b[abs(a[i] - a[i - 1])]++;
+		}
+	}
+
+	bool flag = true;
+	for (int i = 1; i < n; i++) {
+		if (b[i] != 1) {
+			flag = false;
+			break;
+		}
+	}
+
+	if (flag) {
+		printf("YES");
+	}
+	else {
+		printf("NO");
+	}
+
+}
