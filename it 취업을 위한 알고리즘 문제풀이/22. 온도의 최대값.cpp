442102f51549166b2a8be0877712624c3a9b3087
Gatsjy <gkswndks123@naver.com>
2020-05-16 23:00:45 +0900

    Create 22. 온도의 최대값.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/22. 온도의 최대값.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n, k, i, sum = 0, max;
+	
+	scanf("%d %d", &n, &k);
+	
+	vector<int> a(n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	// 첫 구간을 누적 시킨다.
+	for (i = 0; i < k; i++) {
+		sum += a[i];
+	}
+
+	max = sum;
+
+	for (i = k; i < n; i++) {
+		sum = sum + (a[i] - a[i - k]);
+		if (sum > max) max = sum;
+	}
+
+	printf("%d", max);
+}
