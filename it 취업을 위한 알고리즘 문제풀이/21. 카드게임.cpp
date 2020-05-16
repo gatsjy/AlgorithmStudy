14eb94a01a6e38e460f1128f5bc8e1e32ebd7a9d
Gatsjy <gkswndks123@naver.com>
2020-05-16 21:58:20 +0900

    Create 21. 카드게임.cpp

---
+++ it 취업을 위한 알고리즘 문제풀이/21. 카드게임.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+
+int a[10];
+int b[10];
+int scorea = 0;
+int scoreb = 0;
+char lstwin = 'D';
+
+int main() {
+
+	for (int i = 0; i < 10; i++) {
+		int input;
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		int input;
+		scanf("%d", &b[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (a[i] == b[i]) {
+			scorea += 1;
+			scoreb += 1;
+		}
+		else if (a[i] > b[i]) {
+			scorea += 3;
+			lstwin = 'A';
+		}
+		else {
+			scoreb += 3;
+			lstwin = 'B';
+		}
+	}
+
+	printf("%d %d\n", scorea, scoreb);
+	if (scorea == scoreb) {
+		if (lstwin != 'D') {
+			printf("%c", lstwin);
+		}
+		else {
+			printf("D");
+		}
+	}
+	else if (scorea > scoreb) {
+		printf("A");
+	}
+	else {
+		printf("B");
+	}
+
+}
