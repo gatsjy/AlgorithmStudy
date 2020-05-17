706ff454e6595c9a8e4ea517fed7ff32c0b5ff27
Gatsjy <gkswndks123@naver.com>
2020-05-17 21:36:57 +0900

    2020-05-17, 한주안, commit

---
+++ it 취업을 위한 알고리즘 문제풀이/23. 연속 부분 증가수열.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+   int i, n, res = -2147000000, cnt = 1;
+
+   scanf("%d", &n);
+   
+   vector<int> v(n + 1);
+
+   for (i = 0; i < n; i++) {
+      scanf("%d", &v[i]);
+   }
+
+   for (i = 1; i < n; i++) {
+      if (v[i - 1] <= v[i]) {
+         cnt++;
+         res = max(cnt, res);
+      }
+      else {
+         cnt = 1;
+      }
+   }
+
+   printf("%d", res);
+}
---
+++ it 취업을 위한 알고리즘 문제풀이/25. 석차 구하기.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+   int i, n;
+   
+   scanf("%d", &n);
+
+   vector<int> v(n + 1);
+
+   for (i = 0; i < n; i++) {
+      scanf("%d", &v[i]);
+   }
+
+   for (int i = 0; i < n; i++) {
+      int cnt = 1;
+      for (int j = 0; j < n; j++) {
+         if (v[i] < v[j])cnt++;
+      }
+      printf("%d ", cnt);
+   }
+
+}
