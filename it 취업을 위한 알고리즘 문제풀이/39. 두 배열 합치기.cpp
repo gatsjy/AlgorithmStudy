36d61dc2c1c228348b49b86dc91e03dfc5df5f73
Gatsjy <gkswndks123@naver.com>
2020-08-30 18:58:29 +0900

    Update 39. 두 배열 합치기.cpp

--- it 취업을 위한 알고리즘 문제풀이/39. 두 배열 합치기.cpp
+++ it 취업을 위한 알고리즘 문제풀이/39. 두 배열 합치기.cpp
@@ -7,30 +7,39 @@ using namespace std;
 int main() {
 
 	int n, m;
-	
-	// 1. 합쳐질 배열
-	vector<int> mergev;
 
-	// 2. 첫번째 배열 삽입
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int input;
-		cin >> input;
-		mergev.push_back(input);
-	}
-
-	// 3. 두번째 배열 삽입
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) cin >> a[i];
 	cin >> m;
-	for (int i = 0; i < m; i++) {
-		int input;
-		cin >> input;
-		mergev.push_back(input);
+	vector<int> b(m);
+	for (int i = 0; i < m; i++) cin >> b[i];
+
+	vector<int> merge;
+	int aidx = 0;
+	int bidx = 0;
+
+	// 1. 일단 한번 돌리기
+	while (aidx < n && bidx < m) {
+		if (a[aidx] > b[bidx]) {
+			merge.push_back(b[bidx++]);
+		}
+		else {
+			merge.push_back(a[aidx++]);
+		}
 	}
 
-	// 4. 정렬
-	sort(mergev.begin(), mergev.end());
+	// 2. a나머지 처리
+	while (aidx < n) {
+		merge.push_back(a[aidx++]);
+	}
 
-	for (int i = 0; i < mergev.size(); i++) {
-		cout << mergev[i] << " ";
+	// 3. b나머지 처리
+	while (bidx < m) {
+		merge.push_back(b[bidx++]);
+	}
+
+	for (int i = 0; i < merge.size(); i++) {
+		printf("%d ", merge[i]);
 	}
 }
